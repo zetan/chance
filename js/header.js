c5fba81bc8d6eb67de18b58e58f18256ac5dc213
zetan <zetan@ebay.com>
2015-07-18 16:10:38 +0800

    shared header with logo

---
+++ js/header.js
@@ -0,0 +1,111 @@
+function writeLogo(){
+    document.write("<div class=\"col-xs-0 logoimg\">");
+    document.write("<img src=\"images/logo.png\" width=\"60\" height=\"55\"></img>");
+    document.write("</div>");
+    document.write("<div class=\"col-xs-2 logo\">");
+    document.write("<h1><a href=\"index.html\"><span>Chance </span>全  流  通</a></h1>");
+    document.write("</div>");
+}
+
+function writeNavHealth(){
+    document.write("<li class=\"active grid\"><a class=\"color1\" href=\"index.html\">Health</a>");
+    document.write("<div class=\"megapanel\">");
+    document.write("<div class=\"row\">");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\">蜂蜜/Honey</a></li>");
+    document.write("<li><a href=\"men.html\">鱼油/Fish Oil</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\">螺旋藻/Seeweed</a></li>");
+    document.write("<li><a href=\"men.html\">Others/其他</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<h4>畅销产品</h4>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</li>");
+}
+
+function writeNavMaternal(){
+    document.write("<li class=\"active grid\"><a class=\"color1\" href=\"index.html\">Maternal&Child</a>");
+    document.write("<div class=\"megapanel\">");
+    document.write("<div class=\"row\">");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\">花王尿布/Diper</a></li>");
+    document.write("<li><a href=\"men.html\">大王尿布/Diper</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\">英国奶粉/Fomular</a></li>");
+    document.write("<li><a href=\"men.html\">澳洲奶粉/Fomular</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<h4>畅销产品</h4>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</li>");
+}
+
+function writeNavElectornics(){
+    document.write("<li class=\"active grid\"><a class=\"color1\" href=\"index.html\">Electornics</a>");
+    document.write("<div class=\"megapanel\">");
+    document.write("<div class=\"row\">");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\">Ipad Air</a></li>");
+    document.write("<li><a href=\"men.html\">Iphone 6/6 Plus</a></li>");
+    document.write("<li><a href=\"men.html\">Iphone 5/5s</a></li>");
+    document.write("<li><a href=\"men.html\">Apple Watch</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\"Kindle</a></li>");
+    document.write("<li><a href=\"men.html\">Kindle Paper White</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("<div class=\"col1\">");
+    document.write("<div class=\"h_nav\">");
+    document.write("<h4>畅销产品</h4>");
+    document.write("<ul>");
+    document.write("<li><a href=\"men.html\"三星Galaxy/Samsung Galaxy</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</div>");
+    document.write("</li>");
+}
+
+function writeNavAbout(){
+    document.write("<li class=\"active grid\"><a class=\"color4\" href=\"about.html\">Company</a></li>");
+}
+
+function writeNavContact(){
+    document.write("<li class=\"active grid\"><a class=\"color6\" href=\"contact.html\">Contact</a></li>");
+}
