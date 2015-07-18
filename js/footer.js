0f77d4eebb853ce0e37641c1260e6c56041d067e
zetan <zetan@ebay.com>
2015-07-18 17:48:47 +0800

    shared footer

---
+++ js/footer.js
@@ -0,0 +1,55 @@
+function writeCompanyAddr(){
+    document.write("<div class=\"col-md-4 box_3\">");
+    document.write("<h3>联系我们</h3>");
+    document.write("<address class=\"address\">");
+    document.write("<p>地址: 香港中环金钟道89号力宝中心一座3202A.</p>");
+    document.write("<dl>");
+    document.write("<dt></dt>");
+    document.write("<dd>电话:<span> 00852 37592476</span></dd>");
+    document.write("<dd>电话:<span> 00852 37582450</span></dd>");
+    document.write("<dd>传真: <span>00852 21100558</span></dd>");
+    document.write("</dl>");
+    document.write("</address>");
+    document.write("<ul class=\"footer_social\">");
+    document.write("<li><a href=\"\"> <i class=\"fb\"> </i> </a></li>");
+    document.write("<li><a href=\"\"><i class=\"tw\"> </i> </a></li>");
+    document.write("<li><a href=\"\"><i class=\"google\"> </i> </a></li>");
+    document.write("<li><a href=\"\"><i class=\"instagram\"> </i> </a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+}
+
+function writeCooperation(){
+    document.write("<div class=\"col-md-4 box_3\">");
+    document.write("<h3>物流合作</h3>");
+    document.write("<ul class=\"list_1\">");
+    document.write("<li><a href=\"#\">顺丰物流</a></li>");
+    document.write("<li><a href=\"#\">DHL</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+}
+
+function writeSupport(){
+    document.write("<div class=\"col-md-4 box_3\">");
+    document.write("<h3>在线支持</h3>");
+    document.write("<ul class=\"list_1\">");
+    document.write("<li><a href=\"#\">常见问题</a></li>");
+    document.write("<li><a href=\"#\">支付渠道</a></li>");
+    document.write("<li><a href=\"#\">取消订单</a></li>");
+    document.write("</ul>");
+    document.write("<ul class=\"list_1\">");
+    document.write("<li><a href=\"#\">在线服务</a></li>");
+    document.write("<li><a href=\"#\">关于我们</a></li>");
+    document.write("<li><a href=\"#\">联系我们</a></li>");
+    document.write("</ul>");
+    document.write("</div>");
+    document.write("<div class=\"clearfix\"> </div>");
+}
+
+function writeCopyright(){
+    document.write("<div class=\"footer_bottom\">");
+    document.write("<div class=\"copy\">");
+    document.write("<p>Copyright © 2015 Chance Financial Group. All Rights Reserved.</p>");
+    document.write("</div>");
+    document.write("</div>");
+}
